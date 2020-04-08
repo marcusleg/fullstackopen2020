5e8834fadf7e529de16d2b84e7f88fd7f677e421
Marcus Legendre <marcus.legendre@gmail.com>
2020-04-08 20:35:07 +0200

    2.3: calculate total number of exercises using reduce()

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Header from './Header'
 import Content from './Content'
 
-const Course = ({ course }) => (
-  <>
-    <Header name={course.name} />
-    <Content course={course} />
-  </>
-)
+const Course = ({ course }) => {
+  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
+  return (
+    <>
+      <Header name={course.name} />
+      <Content course={course} />
+      <b>total of {total} exercises</b>
+    </>
+  )
+}
 
 export default Course
