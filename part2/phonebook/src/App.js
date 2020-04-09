588e385f914ce11fd031aea0fea932c1ccb19777
Marcus Legendre <marcus.legendre@gmail.com>
2020-04-09 18:53:23 +0200

    2.11: fetch data from json-server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import AddContactForm from './components/AddContactForm'
 import Contacts from './components/Contacts'
 import Filter from './components/Filter'
 
 const App = () => {
   const [ filter, setFilter] = useState('')
-  const [ contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ contacts, setContacts] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/contacts')
+      .then(response => {
+        setContacts(response.data)
+      })
+  }, [])
 
   const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
 
