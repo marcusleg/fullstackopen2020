d1969ab06e1a4e0872f1734406e200883da10558
Marcus Legendre <marcus.legendre@gmail.com>
2020-04-19 19:38:14 +0200

    3.8: log request body for POST request

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 
+morgan.token('body', function (req, res) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let contacts = [
   {
