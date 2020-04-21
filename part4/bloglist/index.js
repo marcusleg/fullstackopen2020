6459c6a5218dc3d2598bc45cd48cbc6f911a378f
Marcus Legendre <marcus.legendre@gmail.com>
2020-04-21 03:51:12 +0200

    4.2: extracted logger utils

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -30,5 +31,5 @@ app.post('/api/blogs', (request, response) => {
 })
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
