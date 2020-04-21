27d9f45828d0b22a8b7748a907aa7f3651ed05de
Marcus Legendre <marcus.legendre@gmail.com>
2020-04-21 05:15:02 +0200

    4.4: totalLikes helper function and tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.map(blog => blog.likes).reduce((totalLikes, likes) => totalLikes + likes)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
