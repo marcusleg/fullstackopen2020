49ff3589cc1513c2a73d1c1bab502b9d9a622d95
Marcus Legendre <marcus.legendre@gmail.com>
2020-04-21 04:02:10 +0200

    4.2: extracted app

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,20 +1,10 @@
+const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
