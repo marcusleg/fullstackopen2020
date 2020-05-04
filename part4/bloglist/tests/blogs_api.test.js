62eb76e5a88b876bd090cc73b52444dea6869285
Marcus Legendre <marcus.legendre@gmail.com>
2020-05-04 02:37:53 +0200

    4.8: API test for GET /api/blogs

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,29 @@
+const helper = require('./test_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const noteObjects = helper.initialBlogs.map(note => new Blog(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
+})
+
+test('all blogs are returned', async() => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
