a156acc5075200a129440239734ac126936e908c
Marcus Legendre <marcus.legendre@gmail.com>
2020-05-18 11:50:03 +0200

    5.1: login form

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs(blogs)
     )
   }, [])
+
+  if (user === null) {
+    return (
+      <>
+        <h2>Log in</h2>
+        <LoginForm user={user} setUser={setUser} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />
+      </>
+    )
+  }
 
   return (
     <div>
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import loginService from '../services/login'
+
+
+const LoginForm = ({user, setUser, username, setUsername, password, setPassword}) => {
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('Wrong credentials')
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+export default LoginForm
