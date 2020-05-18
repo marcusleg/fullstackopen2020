cf5300875dfb469dbcf95b23581ec914e9d3d88d
Marcus Legendre <marcus.legendre@gmail.com>
2020-05-18 11:43:25 +0200

    Morgan logging middleware for bloglist backend

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,6 +9,15 @@ const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+
+morgan.token('body', function (req) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
 
 mongoose.connect(config.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => logger.info('connected to MongoDB'))
@@ -16,6 +25,7 @@ mongoose.connect(config.MONGODB_URI, { useCreateIndex: true, useNewUrlParser: tr
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
