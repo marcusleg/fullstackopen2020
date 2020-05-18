97bf8fbb67ff2e3eaa1926cf3e7b7439347edb09
Marcus Legendre <marcus.legendre@gmail.com>
2020-05-18 17:56:38 +0200

    5.7: togglable blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => setExpanded(!expanded)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}
+      {
+        expanded ?
+          <>
+            <button onClick={toggleExpanded}>hide</button><br />
+            <a href={blog.url}>{blog.url}</a><br />
+            likes {blog.likes}<br />
+            user {blog.user.name}
+          </>
+          :
+          <button onClick={toggleExpanded}>view</button>
+      }
+
+    </div>
+  )
+}
 
 export default Blog
