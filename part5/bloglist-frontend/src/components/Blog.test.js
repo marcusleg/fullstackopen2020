1ef41bacd6ad1743ef174dd324c772f9a2e342f1
Marcus Legendre <marcus.legendre@gmail.com>
2020-05-20 09:07:09 +0200

    5.14: test that detailed content is rendered on button click

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
+test('renders basic content at first', () => {
   const blog = {
-    user: 'Henry Ford',
+    user: {
+      name: 'Henry Ford',
+    },
     likes: 13,
     author: 'Bob Ross',
     title: 'The Joy of Painting',
@@ -26,6 +28,38 @@ test('renders content', () => {
     'http://example.org'
   )
   expect(component.container).not.toHaveTextContent(
+    'user Henry Ford'
+  )
+})
+
+test('renders detailed content when "show" button is clicked', () => {
+  const blog = {
+    user: {
+      name: 'Henry Ford',
+    },
+    likes: 13,
+    author: 'Bob Ross',
+    title: 'The Joy of Painting',
+    url: 'http://example.org',
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    'The Joy of Painting by Bob Ross'
+  )
+  expect(component.container).toHaveTextContent(
+    'likes 13'
+  )
+  expect(component.container).toHaveTextContent(
+    'http://example.org'
+  )
+  expect(component.container).toHaveTextContent(
     'user Henry Ford'
   )
 })
