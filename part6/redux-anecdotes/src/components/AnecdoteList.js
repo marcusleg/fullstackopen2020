3e8a00c6ae972ef55aab42b831211d8827fa38ed
Marcus Legendre <marcus.legendre@gmail.com>
2020-06-04 10:39:03 +0200

    6.12: anecdote filtering

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -5,6 +5,7 @@ import { setNotification, removeNotification } from '../reducers/notificationRed
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   const vote = (id, content) => {
@@ -14,19 +15,24 @@ const AnecdoteList = () => {
     setTimeout(() => dispatch(removeNotification()), 5000)
   }
 
+  let filteredAnecdotes = anecdotes
+  if (filter !== '') filteredAnecdotes = anecdotes.filter(value => value.content.includes(filter))
+
   return (
     <>
-      {anecdotes.sort((a, b) => a.votes < b.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {filteredAnecdotes
+        .sort((a, b) => a.votes < b.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
-          </div>
-        </div>
-      )}
+        )}
     </>
   )
 }
