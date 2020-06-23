3c4e96a78145d7df93cbab3da6981889dc776e2d
Marcus Legendre <marcus.legendre@gmail.com>
2020-06-23 15:35:28 +0200

    7.7: useCountry hook

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,20 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  useEffect(() => {
+    if (name === '') return
+    axios
+      .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+      .then(response => {
+        setCountry({
+          found: true,
+          data: response.data[0]
+        })
+      })
+      .catch(error => {
+        if (error.response.status === 404) setCountry({ found: false })
+      })
+  }, [name])
 
   return country
 }
@@ -40,8 +53,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>
     </div>
   )
 }
